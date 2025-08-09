3158061a4f2dc36e8f1fdaaa9dcc5ba289cf7b5d
Saurabh Mishra <115180665+saurabh43263458@users.noreply.github.com>
2025-08-09 23:02:37 +0530

    Update Start.jsx

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -249,7 +249,7 @@ async function createRide() {
     
   return (
     <div className='h-screen relative '>
-             {!panel && <div ref={profilerRef} className=" absolute right-0 top-[-110px] z-1  translate-y-full  px-3 py-6 "> 
+             {!panel && <div className=" absolute right-0 top-[-110px] z-1  translate-y-full  px-3 py-6 "> 
               <UserLogout user={user} />
             </div>}
             
